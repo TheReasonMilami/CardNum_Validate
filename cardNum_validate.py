0b14251e236aa4e9f6b9fda68e6fac20891319fb
TheReasonMilami <quangminhnv@outlook.com>
2024-01-12 23:02:27 +0700

    push main code

---
+++ cardNum_validate.py
@@ -0,0 +1,27 @@
+def checkLuhn(cardNo):
+    nDigits = len(cardNo)
+    nSum = 0
+    isSecond = False
+    for i in range(nDigits - 1, -1, -1):
+        d = ord(cardNo[i]) - ord('0')
+        if (isSecond == True):
+            d = d*2
+        
+        nSum += d//10
+        nSum += d%10
+        
+        isSecond = not isSecond
+        if (nSum % 10 == 0):
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    cardNo = '79927398713'
+    if (checkLuhn(cardNo)):
+        print("this is valid card")
+    else:
+        print("this is not a valid card")
+
+
+
